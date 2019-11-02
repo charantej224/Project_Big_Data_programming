0eb7aa5cf7ec76b63f2968883b45072b7d2fc008
charantej224 <charantej.career@gmail.com>
2019-11-02 18:29:27 -0500

    collections

--- python_workspace/apps/extract.py
+++ python_workspace/apps/extract.py
@@ -1,19 +1,12 @@
-import re
-
 tempTxt = ""
 # Open if file exists/Create a file to append data
 txt = open('Writetweetsdata.txt', 'a+')
 
 with open('Writetweetsdata.csv', newline='') as csvfile:
     for line in csvfile:
-        tempTxt = ""
-        extractedData = re.findall(r'[#@][^\s#@]+', line)
-        extractedData += re.findall("(?P<url>https?://[^\s]+)", line)
-        print("extracting under process")
-        for data in extractedData:
-            tempTxt += " " + data
-            txt.write(tempTxt)
+        if len(line.split(",")) == 6:
+            txt.write(line)
+        else:
+            print(line + "\n" + str(len(line.split(','))))
 
-print("Sucessfully extracted")
-# close file
 txt.close()
--- python_workspace/apps/tweet_collection.py
+++ python_workspace/apps/tweet_collection.py
@@ -1,6 +1,7 @@
-import json
-import tweepy
 import base64
+import json
+
+import tweepy
 
 # Go to http://apps.twitter.com and create an app.
 # The consumer key and secret will be generated for you after
@@ -9,18 +10,17 @@ f = open('config.json', 'r')
 config = json.load(f)
 
 # Keys secured.
-#consumer_key = config['consumer_key']
-#consumer_secret = config['consumer_secret']
+# consumer_key = config['consumer_key']
+# consumer_secret = config['consumer_secret']
 consumer_key = base64.b64decode(config['consumer_key1']).decode("utf-8")
 consumer_secret = base64.b64decode(config['consumer_secret1']).decode("utf-8")
 
 # After the step above, you will be redirected to your app's page.
 # Create an access token under the the "Your access token" section
-#access_token = config['access_token']
-#access_token_secret = config['access_token_secret']
+# access_token = config['access_token']
+# access_token_secret = config['access_token_secret']
 access_token = base64.b64decode(config['access_token1']).decode("utf-8")
 access_token_secret = base64.b64decode(config['access_token_secret1']).decode("utf-8")
-
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
@@ -29,6 +29,7 @@ auth.set_access_token(access_token, access_token_secret)
 csvFile = open('Writetweetsdata.csv', 'a')
 api = tweepy.API(auth)
 public_tweets = api.home_timeline()
+
 
 class MyStreamListener(tweepy.StreamListener):
 
@@ -46,34 +47,23 @@ class MyStreamListener(tweepy.StreamListener):
             return False
 
     def form_string(self, status):
-        hash_tags = ""
-        urls = ""
-        symbols = ""
-        for hash_tag in status.entities['hashtags']:
-            hash_tags += "$" + hash_tag
-        for url in status.entities['urls']:
-            urls += "$" + url
-        for symbol in status.entities['symbols']:
-            symbols += "$" + symbol
         author = self.clean_string(status.author.name)
         author_location = self.clean_string(status.author.location)
         about_author = self.clean_string(status.author.description)
         created_at = status.created_at
         source = self.clean_string(status.source)
         tweet_message = self.clean_string(status.text)
-        hash_tags = self.clean_string(hash_tags)
-        urls = self.clean_string(urls)
-        symbols = self.clean_string(symbols)
         final = author + "," + author_location + "," + about_author + "," + str(
-            created_at) + "," + source + "," + tweet_message + "," + hash_tags + "," + urls + "," + symbols
+            created_at) + "," + source + "," + tweet_message
         print(final)
         if final:
             csvFile.write(final + "\n")
 
     def clean_string(self, input):
         if input is not None:
-            return_string = input.encode("ascii", errors="ignore").decode().replace('[,"]', '').replace('\n', '')
-            return_string.strip().replace('["]', '')
+            return_string = input.encode("ascii", errors="ignore").decode().replace('\n', '')
+            return_string = return_string.strip().replace('["]', '')
+            return_string = return_string.replace(',', '')
             return return_string
         else:
             return ""
